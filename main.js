00ebc41126932f023590be2d6bed54ad0f77f3ee
Michael Adams <mtadams007@gmail.com>
2018-03-16 16:45:05 -0400

    made some changes to streamline

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+const body = document.body;
 
 class Classic {
   constructor(art_url = 'https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg') {
@@ -9,26 +9,21 @@ class Classic {
     let img = document.createElement('img');
     img.src=this.art_url;
     img.className = this.frame;
-    document.body.appendChild(img);
+    body.appendChild(img);
 
   }
 }
 
-var art1 = new Classic("http://simpleabstract.com/assets/images/Oil-On-Canvas-Abstract-Art.jpg");
+let art1 = new Classic("http://simpleabstract.com/assets/images/Oil-On-Canvas-Abstract-Art.jpg");
 
 class Meme extends Classic {
   constructor(art_url) {
-    super();
-    this.art_url = art_url;
+    super(art_url);
     this.frame = 'chartreuse-border';
   }
-  //
-  // render(){
-  //   super.render();
-  // }
 }
 
-var meme1 = new Meme("http://i0.kym-cdn.com/entries/icons/original/000/022/940/spongebobicon.jpg")
-meme1.render();
+art1.render();
 
-// art1.render();
+let meme1 = new Meme("http://i0.kym-cdn.com/entries/icons/original/000/022/940/spongebobicon.jpg")
+meme1.render();
